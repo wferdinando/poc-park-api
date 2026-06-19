d0b6c5e723756241acd157bb760cd2a0278300b5
Willyan Trautwein Ferdinando <willyantferdinando@gmail.com>
2026-06-19 17:54:05 -0300

    Inicial

---
+++ src/main/java/com/wferdinando/poc_park_api/PocParkApiApplication.java
@@ -0,0 +1,13 @@
+package com.wferdinando.poc_park_api;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class PocParkApiApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(PocParkApiApplication.class, args);
+	}
+
+}
