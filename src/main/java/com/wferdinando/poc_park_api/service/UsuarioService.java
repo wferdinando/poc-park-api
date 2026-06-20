c0843d01984747821c246ff552ddd609931ec92e
Willyan Trautwein Ferdinando <willyantferdinando@gmail.com>
2026-06-20 14:32:08 -0300

    Corrige Injecao no UsuarioService e cria o recurso de create no UsuarioController

--- src/main/java/com/wferdinando/poc_park_api/service/UsuarioService.java
+++ src/main/java/com/wferdinando/poc_park_api/service/UsuarioService.java
@@ -1,15 +1,23 @@
 package com.wferdinando.poc_park_api.service;
 
 import org.springframework.stereotype.Service;
+import org.springframework.transaction.annotation.Transactional;
+
+import com.wferdinando.poc_park_api.entity.Usuario;
+import com.wferdinando.poc_park_api.repository.UsuarioRepository;
 
 @Service
 public class UsuarioService {
-    
-    private final UsuarioService service;
 
-    public UsuarioService(UsuarioService service) {
-        this.service = service;
+    private final UsuarioRepository repository;
+
+    public UsuarioService(UsuarioRepository repository) {
+        this.repository = repository;
     }
 
-    
+    @Transactional
+    public Usuario salvar(Usuario usuario) {
+        return repository.save(usuario);
+    }
+
 }
--- src/main/java/com/wferdinando/poc_park_api/web/controller/UsuarioController.java
+++ src/main/java/com/wferdinando/poc_park_api/web/controller/UsuarioController.java
@@ -1,8 +1,13 @@
 package com.wferdinando.poc_park_api.web.controller;
 
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
+import com.wferdinando.poc_park_api.entity.Usuario;
 import com.wferdinando.poc_park_api.service.UsuarioService;
 
 @RestController
@@ -15,6 +20,10 @@ public class UsuarioController {
         this.service = service;
     }
 
-    
+    @PostMapping
+    public ResponseEntity<Usuario> create(@RequestBody Usuario usuario) {
+        Usuario usuarioSalvo = service.salvar(usuario);
+        return ResponseEntity.status(HttpStatus.CREATED).body(usuarioSalvo);
+    }
 
 }
